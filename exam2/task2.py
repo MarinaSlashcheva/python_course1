4bfc3113030ca868f3d721430f6ce54ff30da9ae
march <slashcheva@gmail.com>
2015-12-05 13:09:45 +0300

    done

---
+++ exam2/task2.py
@@ -0,0 +1,19 @@
+import re
+import requests
+
+with open('links.txt', 'r') as f:
+    text = f.read()
+links = text.split('\n')
+contacts = []
+for link in links:
+    page = requests.get(link).text
+    emails = re.findall('[\w.]+@[\w.]+', page)
+    for adress in emails:
+        if adress not in contacts:
+            contacts.append(adress)
+
+email_list = open('email_adresses.txt', 'w')
+for email in contacts:
+    email_list.write(email)
+    email_list.write('\n')
+email_list.close()
---
+++ exam2/task3.py
@@ -0,0 +1,17 @@
+import requests
+from lxml import etree
+
+url = "https://twitter.com/googlefacts"
+data = requests.get(url).text
+
+parser = etree.HTMLParser()
+tree = etree.fromstring(data, parser)
+
+tweets = open('twitter.txt', 'w')
+for element in tree.iter("p"):
+    if 'class' in element.attrib:
+        if element.attrib['class'] == "TweetTextSize TweetTextSize--16px js-tweet-text tweet-text":
+            tweets.write(element.text)
+            tweets.write('\n')
+
+tweets.close()

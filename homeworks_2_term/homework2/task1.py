87fe9d9a9193984b14c5f9c33785df6366fcbdc4
march <slashcheva@gmail.com>
2016-02-24 01:57:07 +0300

    done

---
+++ homeworks_2_term/homework2/task1.py
@@ -0,0 +1,8 @@
+def sum(a, b):
+    if type(a) is not int or type(b) is not int:
+        raise TypeError
+    else:
+        if a < 0 or b < 0:
+            raise ValueError
+        else:
+            return int(a) + int(b)
